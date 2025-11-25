b943e61992eae8f662acb29f77eb660f86661295
cara <cara091222@gmail.com>
2025-11-25 14:33:18 +0800

    favicon更新

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/1.1.0/modern-normalize.min.css' },
       { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css' },
     ],
